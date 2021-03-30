ad8ccf76e21208bac6d487368cf75530d1d67248
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-03-31 00:09:38 +0200

    Handle Fullscreen resize

--- script.js
+++ script.js
@@ -30,12 +30,23 @@ window.addEventListener('load', function () {
         document.getElementsByTagName('button')[0].textContent = "Navigateur incompatible ou application déjà installée";
     }, 5000);
 
+    let fullscreenMode = false;
+
+    document.getElementsByTagName('iframe')[0].contentDocument.getElementById('action-fullscreen').addEventListener('click', function () {
+        fullscreenMode = !fullscreenMode;
+        if (fullscreenMode) {
+            let overHeight = window.outerHeight - document.getElementsByTagName('iframe')[0].offsetHeight;
+            let calculatorHeight = document.getElementsByTagName('iframe')[0].contentDocument.body.getElementsByClassName('calculator')[0].clientHeight;
+            window.resizeTo(window.outerWidth, calculatorHeight + overHeight);
+        } else {
+            let overHeight = window.outerHeight - document.getElementsByTagName('iframe')[0].offsetHeight;
+            window.resizeTo(parseInt((screen.availHeight - overHeight) * 1160 / 2220), screen.availHeight);
+        }
+    });
+
 });
 
 window.addEventListener('DOMContentLoaded', function () {
-
     let overHeight = window.outerHeight - document.getElementsByTagName('iframe')[0].offsetHeight;
-
     window.resizeTo(parseInt((screen.availHeight - overHeight) * 1160 / 2220), screen.availHeight);
-
 });
