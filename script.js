8d2d2d3922e4a3c662875cd74ae1f8f942d5c52b
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2021-03-30 21:05:59 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+let timeout;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    document.getElementsByTagName('button')[0].classList.remove('wait');
+    document.getElementsByTagName('button')[0].textContent = "Installer l'application";
+    clearTimeout(timeout);
+});
+window.addEventListener('load', function () {
+
+    if (location.protocol !== 'file:') {
+        navigator.serviceWorker.register('sw.js');
+    }
+
+    document.getElementsByTagName('button')[0].addEventListener('click', async function () {
+        if (deferredPrompt) {
+            deferredPrompt.prompt();
+            const { outcom } = await deferredPrompt.userChoice;
+            console.log(outcom);
+        }
+    });
+
+    timeout = setTimeout(function(){
+        document.getElementsByTagName('button')[0].textContent = "Navigateur incompatible ou application déjà installée";
+    }, 5000);
+});
---
+++ sw.js
@@ -0,0 +1,43 @@
+let cacheName = 'numworks-7bbfb555';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './simulator.html',
+                './epsilon.js',
+                './background.jpg',
+                './favicon.ico',
+                './favicon.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            if (r) {
+                return r; // Le fichier est dans le cache, on l'utilise
+            } else {
+                const response = await fetch(e.request);
+                const cache = await caches.open(cacheName);
+                await cache.put(e.request, response.clone()); // On ajoute le fichier au cache
+                return response;
+            }
+        })()
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil((async () => {
+      const keyList = await caches.keys();
+      await Promise.all(keyList.map((key) => {
+        if (key === cacheName) { return; }
+        await caches.delete(key);
+      }))
+    })());
+  });
